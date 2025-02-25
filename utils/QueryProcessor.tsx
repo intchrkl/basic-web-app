6037478d640b8d5a8b538d3aae7689132d4e78d0
intchrkl <intat.tochirakul@gmail.com>
2025-02-25 10:00:19 -0500

    max query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -27,6 +27,13 @@ export default function QueryProcessor(query: string): string {
     return (num1 + num2).toString();
   }
 
+  const maxMatch = query.toLowerCase().match(/which of the following nubmers is the largest: (\d+), (\d+), (\d+)\?/);
+  if (plusMatch) {
+    const num1 = parseInt(plusMatch[1], 10);
+    const num2 = parseInt(plusMatch[2], 10);
+    const num3 = parseInt(plusMatch[3], 10);
+    return Math.max(num1, num2, num3).toString();
+  }
   
   return "";
 }
